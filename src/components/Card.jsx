0d68194f21aee0b3ea09d2c4feb734a27339d7e5
Kabindra Bohara <kabindrabohara1@gmail.com>
2026-05-22 13:35:53 +0530

    add a portfolio project inside projects page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import './Components.css'
 
 const Card = (props) => {
+
+    let view;
+    if(props.id === '1'){
+        view = 0;
+    }
+    else{
+        view = 1;
+    }
+    
     return (
         <div className="card-main">
             <br />
             <div className="image">
 
-                <img src={props.logo} alt="" />
+                <img src={props.logo} alt="project snapshot" />
             </div>
 
             <div className="details">
@@ -16,7 +25,9 @@ const Card = (props) => {
                 <h3>Created on: {props.date}</h3>
                 <h5>{props.desc}</h5>
                 <h5>{props.ver}</h5>  <br/>
-                <div className="link">
+                <div className={view ? 'link': 'disabled-link link'} style={{
+                backgroundColor: view ? '#095285' : 'grey'
+            }}>
 
                     <a href={props.link} target='_blank'>VIEW</a>
                 </div>
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -6,6 +6,7 @@ const Project = () => {
 
   const projects=[
     {
+      id:'11',
       project_name:'Basic Text Game',
       project_date: '08-05-2026',
       project_logo:'/basic-text-game-app-logo.png',
@@ -14,6 +15,7 @@ const Project = () => {
       project_link:'https://text-game-app.vercel.app/'
     },
     {
+      id:'10',
       project_name:'Basic Text Game',
       project_date: '08-05-2026',
       project_logo:'/basic-text-game-app-logo.png',
@@ -22,6 +24,7 @@ const Project = () => {
       project_link:'https://text-game-app.vercel.app/'
     },
     {
+      id:'9',
       project_name:'Basic Text Game',
       project_date: '08-05-2026',
       project_logo:'/basic-text-game-app-logo.png',
@@ -30,6 +33,7 @@ const Project = () => {
       project_link:'https://text-game-app.vercel.app/'
     },
     {
+      id:'8',
       project_name:'Basic Text Game',
       project_date: '08-05-2026',
       project_logo:'/basic-text-game-app-logo.png',
@@ -38,6 +42,7 @@ const Project = () => {
       project_link:'https://text-game-app.vercel.app/'
     },
     {
+      id:'7',
       project_name:'Basic Text Game',
       project_date: '08-05-2026',
       project_logo:'/basic-text-game-app-logo.png',
@@ -46,6 +51,7 @@ const Project = () => {
       project_link:'https://text-game-app.vercel.app/'
     },
     {
+      id:'6',
       project_name:'Basic Text Game',
       project_date: '08-05-2026',
       project_logo:'/basic-text-game-app-logo.png',
@@ -54,6 +60,7 @@ const Project = () => {
       project_link:'https://text-game-app.vercel.app/'
     },
     {
+      id:'5',
       project_name:'Basic Text Game',
       project_date: '08-05-2026',
       project_logo:'/basic-text-game-app-logo.png',
@@ -62,6 +69,7 @@ const Project = () => {
       project_link:'https://text-game-app.vercel.app/'
     },
     {
+      id:'4',
       project_name:'Basic Text Game',
       project_date: '08-05-2026',
       project_logo:'/basic-text-game-app-logo.png',
@@ -70,6 +78,7 @@ const Project = () => {
       project_link:'https://text-game-app.vercel.app/'
     },
     {
+      id:'3',
       project_name:'Basic Text Game',
       project_date: '08-05-2026',
       project_logo:'/basic-text-game-app-logo.png',
@@ -78,6 +87,7 @@ const Project = () => {
       project_link:'https://text-game-app.vercel.app/'
     },
     {
+      id:'2',
       project_name:'Basic Text Game',
       project_date: '08-05-2026',
       project_logo:'/basic-text-game-app-logo.png',
@@ -86,14 +96,16 @@ const Project = () => {
       project_link:'https://text-game-app.vercel.app/'
     },
     {
-      project_name:'Basic Text Game',
-      project_date: '08-05-2026',
-      project_logo:'/basic-text-game-app-logo.png',
-      project_desc:'This is a simple text converter app. It can do basic text operations like reversing, size increasing and decreasing, upper, and lower conversions.',
-      project_version: 'v1.0.0',
-      project_link:'https://text-game-app.vercel.app/'
+      id:'1',
+      project_name:'Personal Portfolio',
+      project_date: '22-05-2026',
+      project_logo:'/portfolio-logo.png',
+      project_desc:'This is a personal site made for myself. It has my projects and resume informations. It displays my skills with reactjs.',
+      project_version: 'v0.1.0',
+      project_link:''
     },
     {
+      id:'0',
       project_name:'Basic Text Game',
       project_date: '08-05-2026',
       project_logo:'/basic-text-game-app-logo.png',
@@ -115,7 +127,7 @@ const Project = () => {
       <div className="card">
 
       {projects.map((project)=>{
-        return <Card name={project.project_name} date={project.project_date} logo={project.project_logo} desc={project.project_desc} ver={project.project_version} link={project.project_link}/>
+        return <Card key={project.id} id={project.id} name={project.project_name} date={project.project_date} logo={project.project_logo} desc={project.project_desc} ver={project.project_version} link={project.project_link}/>
       })}
       </div>
 
